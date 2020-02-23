fcf4617b2d29184bcb79e1466890b31f98c81ac9
Esteban <esteban@pipo.org>
2020-02-23 09:39:57 +0100

    Fix lint errors

--- src/app/pages/cicd/cicd-create-project-page/cicd-create-project-page.component.ts
+++ src/app/pages/cicd/cicd-create-project-page/cicd-create-project-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, ViewChild, Output, Input, EventEmitter, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewInit, ViewChild, Output, Input, EventEmitter, ChangeDetectorRef } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTable, MatTableDataSource } from '@angular/material/table';
@@ -14,7 +14,7 @@ import { ProjectStatus } from '../../../sample/projectstatus.model';
     templateUrl: './cicd-create-project-page.component.html',
     styleUrls: ['./cicd-create-project-page.component.scss'],
 })
-export class CicdCreateProjectPageComponent implements OnInit, AfterViewInit {
+export class CicdCreateProjectPageComponent implements OnInit, OnDestroy, AfterViewInit {
 
     public displayedColumns = ['id', 'name', 'step1', 'step2', 'step3'];
     public dataSource = new MatTableDataSource<ProjectStatus>();
@@ -25,7 +25,7 @@ export class CicdCreateProjectPageComponent implements OnInit, AfterViewInit {
     private subscription: Subscription;
     @Output() TimerExpired: EventEmitter<any> = new EventEmitter<any>();
     @Input() SearchDate: moment.Moment = moment();
-    @Input() ElapsTime: number = 1;
+    @Input() ElapsTime = 1;
 
     searchEndDate: moment.Moment;
     remainingTime: number;
@@ -35,7 +35,7 @@ export class CicdCreateProjectPageComponent implements OnInit, AfterViewInit {
     everySecond: Observable<number> = timer(0, 1000);
 
     constructor(private sampleService: SampleService, private ref: ChangeDetectorRef) {
-        this.searchEndDate = this.SearchDate.add(this.ElapsTime, "minutes");
+        this.searchEndDate = this.SearchDate.add(this.ElapsTime, 'minutes');
     }
 
     ngOnInit() {
@@ -63,14 +63,14 @@ export class CicdCreateProjectPageComponent implements OnInit, AfterViewInit {
 
     refreshPage() {
         this.subscription = this.everySecond.subscribe((seconds) => {
-            var currentTime: moment.Moment = moment();
+            const currentTime: moment.Moment = moment();
             this.remainingTime = this.searchEndDate.diff(currentTime)
             this.remainingTime = this.remainingTime / 1000;
 
             if (this.remainingTime <= 0) {
                 this.refreshTable();
                 this.SearchDate = moment();
-                this.searchEndDate = this.SearchDate.add(this.ElapsTime, "minutes");
+                this.searchEndDate = this.SearchDate.add(this.ElapsTime, 'minutes');
 
                 this.TimerExpired.emit();
             }
